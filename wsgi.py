4d20b7382ee47a7c95bdfcfb3c0d31c33d0327c2
nolove1337-sourse <cibenkomaksim175@gmail.com>
2025-03-09 17:29:32 +0300

    Update deployment configuration for better compatibility

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,7 @@
+import eventlet
+eventlet.monkey_patch()
+
 from nolove_server_8000 import app, socketio
 
 if __name__ == "__main__":
-    socketio.run(app) 
+    socketio.run(app, host='0.0.0.0', port=8000) 
